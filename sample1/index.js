f49771aaa587068cb804331d56a56d8bf4b3b685
Sohaib Nehal <sohaib.nehal@ymail.com>
2018-02-13 11:21:48 +0500

    sample 1 initial commit

---
+++ sample1/index.js
@@ -0,0 +1,6 @@
+
+require('./main.css');
+// import another component
+import { hello } from './main';
+
+hello('Sohaib Nehal');
---
+++ sample1/main.js
@@ -0,0 +1,8 @@
+
+const log = console.log;
+
+module.exports = {
+
+    hello: name => log(name)
+
+};
